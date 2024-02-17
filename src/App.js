f0b869767b709fc2edb0818d3e8514289f484567
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-02-18 00:01:26 +0300

    Обновление результатов.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 
+const wordList = [
+  "processor", "memory", "microchip", "network", "keyboard",
+  "mouse", "monitor", "printer", "disk", "adversary",
+  "person", "tennis", "jungle", "storage", "suspension",
+  "dolphin", "snowman", "reign", "quiz", "gene", 
+  "ping", "clarity", "compassion", "barber", "mirror", 
+  "dinner", "savage", "petrol", "piracy", "tracer"
+];
+
 function App() {
   return <>
     <h1>Route Home Work</h1>
---
+++ src/components/navmenu/index.jsx
@@ -0,0 +1,9 @@
+import {NavLink} from "react-router-dom";
+
+const Menu = () => {
+    return <nav>
+        <NavLink to="/">Главная страница</NavLink>
+    </nav>
+}
+
+export default Menu;
