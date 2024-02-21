0eefa4707e8cf40350e6aacabf5e7da4b0d9be14
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-02-21 19:50:21 +0300

    добавление компонента редактирования данных.

--- src/components/WordData/WordData.jsx
+++ src/components/WordData/WordData.jsx
@@ -2,9 +2,15 @@ import { NavLink } from "react-router-dom";
 
 import "./WordData.css"
 
-const WordData = ({value, linkData}) => {
-    return <div className="worddata-item">
-        <NavLink to={linkData}>{value}</NavLink>
+const WordData = ({value, linkData}) => { 
+    return <div className="worddata">
+        <div className="worddata__item">
+            <NavLink to={linkData}>{value}</NavLink>
+        </div>
+        <div className="worddata__edit">
+            <input type="text" className=""/>
+            <button>Изменить</button>
+        </div>
     </div>
 }
 
