d17bc1ef58d9e6c8aa76de43982c049de2d4b98f
Alex <42239536+RobertAnderson1985@users.noreply.github.com>
2024-02-23 10:42:16 +0300

    обновление результатов.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
+
 import progdata from "../assets/datas/progdata.json";
+import addIcon from "../assets/images/add.svg";
+import okIcon from "../assets/images/ok.svg";
+import cancelItem from "../assets/images/cancel.svg";
 
 import WordData from "../components/WordData/WordData";
 
-
 const Home = () =>{
     
+    const [showPanel, setAddEditPanel] = useState(false);
+    const [newWord, setNewWord] = useState("");
+
     const words = progdata.words.sort();
+
+    const showAddEditPanel = () => { setAddEditPanel(true)}
+
+    const hideAddEditPanel = () => {
+        setAddEditPanel(false);
+        setNewWord("");
+    }
 
     return <main>
         <h1>Главная страница</h1>
+        <section className="editpanel"> 
+            {showPanel && 
+                <div className="worddata__edit">
+                <input type="text" className="worddata__edit-tfield"/>
+                <button className="worddata__edit-button">
+                    <img className="worddata__edit-icon" src={okIcon} alt="" />
+                </button>
+                <button className="worddata__edit-button" onClick={hideAddEditPanel}>
+                    <img className="worddata__edit-icon" src={cancelItem} alt="" />
+                </button>
+            </div>
+            }
+            <button className="button__add"><img className="button__add-icon" src={addIcon} alt="" onClick={showAddEditPanel} /></button>
+        </section>
         <section>
             {
                 words.map((item, index) => 
